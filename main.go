953428429bd7dfa27787c9e71589887d391559d2
GSujiKanth <119513869+GSujiKanth@users.noreply.github.com>
2024-02-05 21:37:56 +0530

    CREATED struct Movie and Director

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"github.com/gorilla/mux"
-	"log"
-	"math/rand"
-	"net/http"
-	"strconv"
-)
+// struct of type movie
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+// Every Movie has a Director
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+// when we send this data using Postman, we'll be using json names
+
+var movies []Movie
