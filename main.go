673c37382a87f3e0c993650c6e03073483803854
GSujiKanth <119513869+GSujiKanth@users.noreply.github.com>
2024-02-06 21:12:13 +0530

    CREATED main function

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+)
+
 // struct of type movie
 type Movie struct {
 	ID       string    `json:"id"`
@@ -17,3 +27,16 @@ type Director struct {
 // when we send this data using Postman, we'll be using json names
 
 var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting serve at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
